be4d28c1dfb74171abde1bea6cc7ea41a88adb4d
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-08-12 14:56:51 -0300

    jjjjçç

---
+++ main.js
@@ -0,0 +1,23 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > i) {
+        tamanhoSenha = tamanhoSenha - 1
+    }
+numeroSenha, textcontent = tamanhoSenha;
+}function aumentaTamanhoTamanho() {
+    if (tamanhoSenha > i) {
+        tamanhoSenha = tamanhoSenha - 1
+    }
+numeroSenha, textcontent = tamanhoSenha;
+}
+
+
+
