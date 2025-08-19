ee0f66be7b3713f3946554ba600c32a8d5515f54
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-08-19 14:30:20 -0300

    KKKK

--- main.js
+++ main.js
@@ -7,21 +7,28 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-function aumentaTamanho(){
-    if(tamanhoSenha < 20){
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha = tamanhoSenha + 1
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
+const camposenha = document.querySelector('#camnposenha');
+
+const letrasMaiúsculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinúsculas = 'abcdefghijklmnopqrstuvwxyz';
+const Números = '0123456789';
+const Símbolos = '#@!?_$%*';
+geraSenha();
 
 
